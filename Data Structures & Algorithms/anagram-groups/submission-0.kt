7d05882899382025372b6420c3962d32f22a2fd2
pirmukhammad khusanov <84397432+pxusanov@users.noreply.github.com>
2026-07-02 15:49:07 +0500

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.kt
@@ -0,0 +1,34 @@
+class Solution {
+
+       private fun isAnagram(s1: String, s2: String): Boolean {
+        val count = IntArray(26)
+        if (s1.length != s2.length) return false
+        for (i in s1.indices) {
+            count[s1[i] - 'a']++
+            count[s2[i] - 'a']--
+        }
+        return count.all { it == 0 }
+    }
+    
+     fun groupAnagrams(strs: Array<String>): List<List<String>> {
+
+        val map = hashMapOf<String, List<String>>()
+        var isAnagrams = false
+
+        strs.forEach { item ->
+            var foundGroup = false
+            for (entry in map) {
+                if (isAnagram(item, entry.key)) {
+                    map[entry.key] = entry.value + item
+                    foundGroup = true
+                    break
+                }
+            }
+            if (!foundGroup) map[item] = listOf(item)
+        }
+
+        return map.values.toList()
+
+    }
+
+}
